464f437fac63b0946a5b4f95b0b0d5b14b6c536d
neeraj.srivastava_india.nec.com <neeraj.srivastava@india.nec.com>
2023-10-07 09:14:25 +0000

    commit initial project

---
+++ StudyAdda/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { CourcesComponent } from './components/cources/cources.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationBarComponent,
+    CourcesComponent,
+    LoginComponent,
+    RegistrationComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
